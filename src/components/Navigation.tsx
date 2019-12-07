f73fd16750892ed8572bf5077f2621b77acbdb9d
Dustin Heckmann <dustin.heckmann@thoughtworks.com>
2019-12-07 15:33:15 +0100

    Add export link to header

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,5 +6,6 @@ export default () => (
   <nav className={styles.navigation}>
     <Link to="/citations">Publications</Link>
     <Link to="/statistics/completeness">Completeness</Link>
+    <Link to="/api/export" target="_blank">Export</Link>
   </nav>
 )
