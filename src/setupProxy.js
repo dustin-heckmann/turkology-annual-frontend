3c11b15b3c0b4607a5a8dd197d792b87b5e3b29a
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-02-22 15:17:40 +0100

    Add Kotlin backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,13 @@
-const proxy = require("http-proxy-middleware");
+const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(
-    proxy("/api", {
-      target: "http://localhost:5000/"
+    proxy(['/api/statistics', '/api/export'], {
+      target: 'http://localhost:5000/'
+    }),
+
+    proxy('/api', {
+      target: 'http://localhost:8080/'
     })
-  );
-};
+  )
+}
