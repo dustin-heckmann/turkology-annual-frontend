38528f7037ee5bb038ddac3ad634bc6656809594
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-03-01 19:00:17 +0100

    Make header color red, pagination buttons wider, use HCTS logo

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import logo from '../img/logo_cluster.png'
+import logo from '../img/logo_hcts.png'
 import styles from './Header.module.css'
 import Navigation from './Navigation'
 import PageTitle from './PageTitle'
