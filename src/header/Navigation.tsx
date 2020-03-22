97751b04b60aed89310683d7c8acb75e7f885f55
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-03-22 13:52:38 +0100

    Serve export from static zip file

--- src/header/Navigation.tsx
+++ src/header/Navigation.tsx
@@ -7,7 +7,7 @@ const Navigation = () => (
     <Link to="/citations">Publications</Link>
     <Link to="/statistics/completeness">Completeness</Link>
     <Link to="/about">About</Link>
-    <Link to="/api/export" target="_blank">
+    <Link to="/export" target="_blank">
       Export
     </Link>
   </nav>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(
-    proxy(['/api/statistics', '/api/export'], {
+    proxy(['/api/statistics'], {
       target: 'http://localhost:5000/'
     }),
 
